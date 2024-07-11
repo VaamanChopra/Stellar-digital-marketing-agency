d9c385024d4e29778bf6ad9894e07d7785931a95
VaamanChopra <vaamanchopra@gmail.com>
2024-07-11 17:43:32 +0530

    initialising header

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>toh chaliye shuru krte</div>
+    <div className="w-full md items-center 
+    md:justify-center bg-black/[0.96] 
+    antialiased bg-grid-white/[0.02] relative 
+    overflow-hidden">toh chaliye shuru krte</div>
   );
 }
