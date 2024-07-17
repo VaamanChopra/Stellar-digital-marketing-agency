7baeab4e160f7390d90a7aa50bfbd2214f96f462
VaamanChopra <vaamanchopra@gmail.com>
2024-07-17 12:57:18 +0530

    Initialising Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div className="w-full md items-center 
-    md:justify-center bg-black/[0.96] 
-    antialiased bg-grid-white/[0.02] relative 
-    overflow-hidden">toh chaliye shuru krte</div>
-  );
+     <div className="w-full md:items-center 
+     md:justify-center bg-black/[0.96] 
+     antialiased bg-grid-white/[0.02] relative 
+     overflow-hidden">
+      <Navbar />
+     </div>
+   );
 }
